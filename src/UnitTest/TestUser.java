460c55b846843cd3af52df254d747f249c48f4f4
Clus Ngo <clusngo@WINDOWS2.SCS.CARLETON.CA>
2017-10-05 12:17:29 -0400

    Add User, TestUser classes
    Add User(int userid, String username, String password), testConstructorPass(), testConstructorFail()

---
+++ src/UnitTest/TestUser.java
@@ -0,0 +1,21 @@
+package UnitTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Server.Logic.Model.User;
+
+public class TestUser {
+	User tester = new User (2, "michelle@carleton.ca","Michelle");
+	User tester1;
+
+	@Test
+	public void testConstructorPass(){
+		assertNotNull(tester);
+	}
+	@Test
+	public void testConstructorFail(){
+		assertNull(tester1);
+	}
+}
