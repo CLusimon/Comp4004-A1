cf64015dbbe2f46c65598f6b7a1cf7fbd367b55a
Clus Ngo <clusngo@windows2.scs.carleton.ca>
2017-10-05 10:51:48 -0400

    Add Loan, TestLoan classes
    Add Loan(int userid, String isbn, String copynumber, Date date, String renewstate), testConstructorPass(), testConstructorFail()

---
+++ src/Server/Logic/Model/Loan.java
@@ -0,0 +1,22 @@
+package Server.Logic.Model;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Loan {
+	int userid;
+	String isbn;
+	String copynumber;
+	Date date;
+	String renewstate;
+	
+	public Loan(int userid,String isbn,String copynumber,Date date,String renewstate){
+		this.userid=userid;
+		this.isbn=isbn;
+		this.copynumber=copynumber;
+		this.date=date;
+		this.renewstate=renewstate;
+	}
+	
+	DateFormat format1 = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+}
---
+++ src/UnitTest/TestLoan.java
@@ -0,0 +1,25 @@
+package UnitTest;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import Server.Logic.Model.Loan;
+
+public class TestLoan {
+	Date date = new Date();
+	Loan tester = new Loan (4,"1234567890123","3",date ,"4");
+	Loan tester1;
+	
+	@Test
+	public void testConstructorPass(){
+		assertNotNull(tester);
+	}
+	@Test
+	public void testConstructorFail(){
+		assertNull(tester1);
+	}
+
+}
