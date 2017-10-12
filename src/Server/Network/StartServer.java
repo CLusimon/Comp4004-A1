7f05987f335e6a72931a6f38f0993770c0818e66
Clus Ngo <clusngo@WINDOWS2.SCS.CARLETON.CA>
2017-10-12 13:09:23 -0400

    Add LibServer, ServerThread, StartServer

---
+++ src/Server/Network/StartServer.java
@@ -0,0 +1,10 @@
+package Server.Network;
+
+import Utilities.Config;
+
+public class StartServer {
+	public static void main(String[] argv) {
+		System.out.println("Starting server ...");
+		new LibServer(Config.DEFAULT_PORT);
+	}
+}
