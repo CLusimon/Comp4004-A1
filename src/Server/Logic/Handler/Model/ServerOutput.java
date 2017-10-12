129b7d8697eabdc24644681582124be135d55fbd
Clus Ngo <clusngo@WINDOWS2.SCS.CARLETON.CA>
2017-10-12 13:05:04 -0400

    Add InputHandler, OutputHandler, Client, Output, ServerOutput

---
+++ src/Server/Logic/Handler/Model/ServerOutput.java
@@ -0,0 +1,26 @@
+package Server.Logic.Handler.Model;
+
+public class ServerOutput {
+	String output;
+	int state;
+	public ServerOutput(String output,int state){
+		this.output=output;
+		this.state=state;
+		
+	}
+	public String getOutput() {
+		return output;
+	}
+	public void setOutput(String output) {
+		this.output = output;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	
+
+}
+
